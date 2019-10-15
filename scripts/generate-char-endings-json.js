1a3952a09616202d8d5f18fe43f9dd690c5ee361
Nhan Bui <imouto1994@gmail.com>
2019-10-15 16:05:45 +0800

    Add scripts & endings JSONs

---
+++ scripts/generate-char-endings-json.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+const path = require("path");
+
+(function() {
+  const content = fs.readFileSync(
+    path.resolve(__dirname, "../json/routeEndings.json"),
+    "utf-8",
+  );
+  const routeEndingsMap = JSON.parse(content);
+  const completeRouteEndingsMap = JSON.parse(content);
+
+  for (const characterA of Object.keys(routeEndingsMap)) {
+    for (const characterB of Object.keys(routeEndingsMap[characterA])) {
+      completeRouteEndingsMap[characterB][characterA] =
+        routeEndingsMap[characterA][characterB];
+    }
+  }
+
+  const charEndingsMap = {};
+  for (const characterA of Object.keys(completeRouteEndingsMap)) {
+    charEndingsMap[characterA] = {};
+    for (const characterB of Object.keys(completeRouteEndingsMap[characterA])) {
+      const endings = completeRouteEndingsMap[characterA][characterB];
+      const endingRoutesMap = endings.reduce((map, ending, index) => {
+        if (ending != null) {
+          if (map[ending] == null) {
+            map[ending] = [];
+          }
+          let route;
+          if (index === 0) {
+            route = "Crimson Flower";
+          } else if (index === 1) {
+            route = "Azure Moon";
+          } else if (index === 2) {
+            route = "Verdant Wind";
+          } else {
+            route = "Silver Snow";
+          }
+
+          map[ending].push(route);
+        }
+
+        return map;
+      }, {});
+      charEndingsMap[characterA][characterB] = Object.keys(endingRoutesMap).map(
+        ending => {
+          return {
+            ending,
+            routes: endingRoutesMap[ending],
+          };
+        },
+      );
+    }
+    fs.writeFileSync(
+      path.resolve(
+        __dirname,
+        `../json/${characterA
+          .split(" ")
+          .join("_")
+          .toLowerCase()}.json`,
+      ),
+      JSON.stringify(charEndingsMap[characterA]),
+      "utf-8",
+    );
+  }
+
+  fs.writeFileSync(
+    path.resolve(__dirname, `../json/charEndings.json`),
+    JSON.stringify(charEndingsMap, null, 2),
+    "utf-8",
+  );
+})();
---
+++ scripts/generate-endings-json.js
@@ -0,0 +1,228 @@
+const fs = require("fs");
+const path = require("path");
+
+const ENDING_HEADER_MAX_LENGTH = 30;
+
+const CHARACTERS = [
+  "Byleth M",
+  "Byleth F",
+  "Edelgard",
+  "Dimitri",
+  "Claude",
+  "Hubert",
+  "Ferdinand",
+  "Linhardt",
+  "Caspar",
+  "Bernadetta",
+  "Dorothea",
+  "Petra",
+  "Dedue",
+  "Felix",
+  "Ashe",
+  "Sylvain",
+  "Mercedes",
+  "Annette",
+  "Ingrid",
+  "Lorenz",
+  "Raphael",
+  "Ignatz",
+  "Lysithea",
+  "Marianne",
+  "Hilda",
+  "Leonie",
+  "Seteth",
+  "Flayn",
+  "Hanneman",
+  "Manuela",
+  "Gilbert",
+  "Alois",
+  "Catherine",
+  "Shamir",
+  "Cyril",
+  "Rhea",
+];
+
+const BYLETH_MALE_PARTNERS = [
+  "Alois",
+  "Annette",
+  "Bernadetta",
+  "Catherine",
+  "Dorothea",
+  "Edelgard",
+  "Flayn",
+  "Gilbert",
+  "Hilda",
+  "Ingrid",
+  "Leonie",
+  "Linhardt",
+  "Lysithea",
+  "Manuela",
+  "Marianne",
+  "Mercedes",
+  "Petra",
+  "Rhea",
+  "Shamir",
+];
+
+const BYLETH_FEMALE_PARTNERS = [
+  "Alois",
+  "Ashe",
+  "Caspar",
+  "Claude",
+  "Cyril",
+  "Dedue",
+  "Dimitri",
+  "Dorothea",
+  "Edelgard",
+  "Felix",
+  "Ferdinand",
+  "Gilbert",
+  "Hanneman",
+  "Hubert",
+  "Ignatz",
+  "Linhardt",
+  "Lorenz",
+  "Mercedes",
+  "Raphael",
+  "Rhea",
+  "Seteth",
+  "Sylvain",
+];
+
+function addEntry(endingsMap, characterA, characterB, ending) {
+  if (endingsMap[characterA] == null) {
+    endingsMap[characterA] = {};
+  }
+  if (endingsMap[characterA][characterB] == null) {
+    endingsMap[characterA][characterB] = [];
+  }
+  if (endingsMap[characterB] == null) {
+    endingsMap[characterB] = {};
+  }
+  if (endingsMap[characterB][characterA] == null) {
+    endingsMap[characterB][characterA] = [];
+  }
+
+  if (CHARACTERS.indexOf(characterA) < CHARACTERS.indexOf(characterB)) {
+    endingsMap[characterA][characterB].push(ending);
+  } else {
+    endingsMap[characterB][characterA].push(ending);
+  }
+}
+
+function addSelfEntry(endingsMap, character, ending) {
+  if (endingsMap[character] == null) {
+    endingsMap[character] = {};
+  }
+  if (endingsMap[character][character] == null) {
+    endingsMap[character][character] = [];
+  }
+  endingsMap[character][character].push(ending);
+}
+
+function cleanUpMainCharEnding(ending, isMale) {
+  if (isMale) {
+    return ending
+      .replace(/S0/g, "Byleth")
+      .replace(/EK00/g, "")
+      .replace(/EL00\w+EM00/g, "");
+  } else {
+    return ending
+      .replace(/S0/g, "Byleth")
+      .replace(/EK00\w+EL00/g, "")
+      .replace(/EM00/g, "");
+  }
+}
+
+(function() {
+  const content = fs.readFileSync(
+    path.resolve(__dirname, "./endings.txt"),
+    "utf-8",
+  );
+
+  const lines = content
+    // Remove all \r characters
+    .replace(/\r/g, "")
+    // Replace all escaped new line with spaces
+    .replace(/\\n/g, " ")
+    // Replace all double spaces with spaces
+    .replace(/  /g, " ")
+    // Remove all escape characters
+    .replace(/\u001b/g, "")
+    // Split into array of strings by \n characters
+    .split("\n")
+    // Trim all strings
+    .map(line => line.trim())
+    // Filter out all empty strings
+    .filter(line => line.length > 0);
+
+  const endingsMap = {};
+  let characterA;
+  let characterB;
+
+  for (const line of lines) {
+    if (line.length < ENDING_HEADER_MAX_LENGTH) {
+      const characters = line.split(" x ");
+      characterA = characters[0].trim();
+      characterB = characters[1] != null ? characters[1].trim() : undefined;
+    } else {
+      // Self Ending
+      if (characterB == null) {
+        addSelfEntry(endingsMap, characterA, line);
+      } else {
+        // Main Character
+        if (characterA === "Byleth M") {
+          addEntry(
+            endingsMap,
+            "Byleth M",
+            characterB,
+            cleanUpMainCharEnding(line, true),
+          );
+        } else if (characterA === "Byleth F") {
+          addEntry(
+            endingsMap,
+            "Byleth F",
+            characterB,
+            cleanUpMainCharEnding(line, false),
+          );
+        } else if (characterA === "Byleth") {
+          // Partner in the list of possible male partners
+          if (BYLETH_MALE_PARTNERS.indexOf(characterB) !== -1) {
+            addEntry(
+              endingsMap,
+              "Byleth M",
+              characterB,
+              cleanUpMainCharEnding(line, true),
+            );
+          }
+
+          // Partner in the list of possible female partners
+          if (BYLETH_FEMALE_PARTNERS.indexOf(characterB) !== -1) {
+            addEntry(
+              endingsMap,
+              "Byleth F",
+              characterB,
+              cleanUpMainCharEnding(line, false),
+            );
+          }
+        } else {
+          addEntry(endingsMap, characterA, characterB, line);
+        }
+      }
+    }
+  }
+
+  for (const characterA of Object.keys(endingsMap)) {
+    for (const characterB of Object.keys(endingsMap[characterA])) {
+      if (!endingsMap[characterA][characterB].length) {
+        delete endingsMap[characterA][characterB];
+      }
+    }
+  }
+
+  fs.writeFileSync(
+    path.resolve(__dirname, "../json/endings.json"),
+    JSON.stringify(endingsMap, null, 2),
+    "utf-8",
+  );
+})();
