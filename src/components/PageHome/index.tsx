25f22c5dadcf5a69c131d211325e94f8660a30df
Nhan Bui <imouto1994@gmail.com>
2019-10-24 19:54:57 +0800

    Remove duplicate orderedCharacters

--- src/components/PageHome/index.tsx
+++ src/components/PageHome/index.tsx
@@ -3,47 +3,9 @@ import { useHistory } from "react-router-dom";
 
 import Page from "../Page";
 import CharacterList from "../CharacterList";
+import { orderedCharacters } from "../../data/character";
 import { Character } from "../../data/character/type";
 import { slugify } from "../../utils/string";
-
-const orderedCharacters: Character[] = [
-  "Byleth M",
-  "Byleth F",
-  "Edelgard",
-  "Dimitri",
-  "Claude",
-  "Hubert",
-  "Ferdinand",
-  "Linhardt",
-  "Caspar",
-  "Bernadetta",
-  "Dorothea",
-  "Petra",
-  "Dedue",
-  "Felix",
-  "Ashe",
-  "Sylvain",
-  "Mercedes",
-  "Annette",
-  "Ingrid",
-  "Lorenz",
-  "Raphael",
-  "Ignatz",
-  "Lysithea",
-  "Marianne",
-  "Hilda",
-  "Leonie",
-  "Seteth",
-  "Flayn",
-  "Hanneman",
-  "Manuela",
-  "Gilbert",
-  "Alois",
-  "Catherine",
-  "Shamir",
-  "Cyril",
-  "Rhea",
-];
 
 export default function PageHome(): ReactElement {
   const history = useHistory();
